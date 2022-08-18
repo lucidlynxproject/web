66e3819b5b151b97d7845f38f0906038000396c4
Carlos Rocholl <rocholl97@gmail.com>
2022-08-18 21:33:09 +0100

    contact form added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,10 @@ import { AppComponent } from './app.component';
 import { PricingComponent } from './components/pricing/pricing.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
-
+import { CountdownComponent } from './components/countdown/countdown.component';
+import { BackRectangleComponent } from './components/back-rectangle/back-rectangle.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +17,10 @@ import { HomeComponent } from './components/home/home.component';
     PricingComponent,
     NavbarComponent,
     HomeComponent,
+    CountdownComponent,
+    BackRectangleComponent,
+    ContactFormComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
